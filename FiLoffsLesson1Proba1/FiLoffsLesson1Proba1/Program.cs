acf231c6047ab0e7de1e75810cbff7ecfedef79c
FiLoffs <filod72@gmail.com>
2019-02-21 20:03:49 +0200

    создал проект, добавил в существующий репозиторий

---
+++ FiLoffsLesson1Proba1/FiLoffsLesson1Proba1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiLoffsLesson1Proba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("создал отдельный проект, чтобы экспортировать в репозиторий");
+        }
+    }
+}
